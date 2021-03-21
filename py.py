cb0da2e96944edf757b7542f9bea834506eedf05
beomsu7 <72853382+beomsu7@users.noreply.github.com>
2021-03-21 18:28:23 +0900

    Update py.py

--- py.py
+++ py.py
@@ -18,35 +18,52 @@ import mavros
 from geometry_msgs.msg import PoseStamped
 from mavros_msgs.srv import SetMode, CommandBool
 
-import sys, select, termios, tty
+import sys, select, termios, tty, math
 
+init_msg="""
+--------------------------
+"POSE CONTORLLER"
+--------------------------
+1. press any key to initialize
+the value
+
+2. if there is big gap from z to 0
+check your mav
+
+3. press 'n' to start
+
+CTRL-C to quit------------
+--------------------------
+$$initial pose$$
+"""
+init = True;
 msg = """
 --------------------------
 "POSE CONTORLLER"
 --------------------------
 offboard            : q
 arming              : e
-landing             : r
-
- +x            +z 
- ^           i  ^             
- |   w          |  
- | a s d     k  v  
- @----> -y     -z
-
+landing or reset    : r
+newvalue            : n
+ +x            +z(i) 
+ ^              ^             
+ |   w      < j | l > 
+ | a s d        v  
+ @----> -y     -z(k)
 CTRL-C to quit------------
 --------------------------
 """
 
 moveBindings = {
-        'w':(1,0,0),
-        's':(-1,0,0),
-        'a':(0,1,0),
-        'd':(0,-1,0),
-        'i':(0,0,1),
-        'k':(0,0,-1),
-
-        'q':(0,0,0), 'e':(0,0,0), 'r':(0,0,0),
+        'w':(1,0,0,0),
+        's':(-1,0,0,0),
+        'a':(0,1,0,0),
+        'd':(0,-1,0,0),
+        'i':(0,0,1,0),
+        'k':(0,0,-1,0),
+        'n':(0,0,0,0),
+        'q':(0,0,0,0), 'e':(0,0,0,0), 'r':(0,0,0,0),
+        'j':(0,0,0,1), 'l':(0,0,0,-1)
 }
 
 
@@ -67,6 +84,7 @@ def arming(bool_):
     """
     arming = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
     arming(value=bool_)
+    
 
 def set_mode(mode):
     """
@@ -82,13 +100,36 @@ def set_mode(mode):
     set_mode = rospy.ServiceProxy('/mavros/set_mode', SetMode)
     set_mode(0, mode)
     
+def callback(msg):
+
+    pub_thread.x = round(msg.pose.position.x,3)
+    pub_thread.y = round(msg.pose.position.y,3)
+    pub_thread.z = round(msg.pose.position.z,3)
+    pub_thread.degree = round(quarterion_to_degree(msg.pose.orientation.z, msg.pose.orientation.w),3)
+ 
+
+    
+def degree_to_quarterion(degree):
+    #the degree should be under 180 and over -180
+    z = math.sin(math.radians(degree/2))
+    w = math.cos(math.radians(degree/2))
+    return z, w
+
+def quarterion_to_degree(z, w):
+    return math.degrees(math.atan2(2*w*z, 1-2*z*z))
+    
+
 class PublishThread(threading.Thread):
     def __init__(self, rate):
         super(PublishThread, self).__init__()
         self.publisher = rospy.Publisher('mavros/setpoint_position/local', PoseStamped, queue_size=10)
+        self.subscriber = rospy.Subscriber('mavros/local_position/pose', PoseStamped, callback)
         self.x = 0.0
         self.y = 0.0
         self.z = 0.0
+        self.Z = 0.0
+        self.W = 0.0
+        self.degree = 0.0
         self.condition = threading.Condition()
         self.done = False
 
@@ -112,18 +153,22 @@ class PublishThread(threading.Thread):
         if rospy.is_shutdown():
             raise Exception("Got shutdown request before subscribers connected")
 
-    def update(self, x, y, z):
+    def update(self, x, y, z, degree):
         self.condition.acquire()
         self.x = x
         self.y = y
         self.z = z
+        self.degree = degree
+        Z, W = degree_to_quarterion(degree)
+        self.Z = Z
+        self.W = W
         # Notify publish thread that we have a new message.
         self.condition.notify()
         self.condition.release()
 
     def stop(self):
         self.done = True
-        self.update(0, 0, 0)
+        self.update(0, 0, 0, 0)
         self.join()
 
     def run(self):
@@ -137,6 +182,8 @@ class PublishThread(threading.Thread):
             pos.pose.position.x = pos_x
             pos.pose.position.y = pos_y
             pos.pose.position.z = pos_z
+            pos.pose.orientation.z = self.Z
+            pos.pose.orientation.w = self.W
 
             self.condition.release()
 
@@ -147,7 +194,11 @@ class PublishThread(threading.Thread):
         pos.pose.position.x = 0
         pos.pose.position.y = 0
         pos.pose.position.z = 0
+        pos.pose.orientation.z = 0
+        pos.pose.orientation.w = 1
         self.publisher.publish(pos)
+        #print("\r" + txt, end="")
+        
 
 
 
@@ -161,26 +212,60 @@ if __name__=="__main__":
 
     repeat = rospy.get_param("~repeat_rate", 20)
     pub_thread = PublishThread(repeat)
-
-    pos_x = 0
-    pos_y = 0
-    pos_z = 0
+    pos_x = 0.0
+    pos_y = 0.0
+    pos_z = 0.0
+    degree = 0.0
+    w = 0.0
+    z = 0.0
     pos = PoseStamped()
-    print(msg)
+    #print(init_msg)
 
     try:
         pub_thread.wait_for_subscribers()
-        pub_thread.update(pos_x, pos_y, pos_z)
+        print(init_msg)
+        while(init):
+            txt = ""
+            txt += 'x : ' + str(pub_thread.x)
+            txt += ', y : ' + str(pub_thread.y)
+            txt += ', z : ' + str(pub_thread.z)
+            txt += ', deg : ' + str(pub_thread.degree)
+            print("\r                                       ", end="")
+            print("\r" + txt, end="")
+            key = getKey(key_timeout)
+            
+            if key == 'n': 
+                init = False
+                pos_x = pub_thread.x
+                pos_y = pub_thread.y
+                degree = pub_thread.degree
+                pub_thread.subscriber.unregister()
+                break
+            else:
+                # stopped.
+                if (key == '\x03'):
+                    break
+        
+        print(msg)
+
+        
+        
+        pub_thread.update(pos_x, pos_y, pos_z, degree)
         while(1):
+            
+            
+            
             key = getKey(key_timeout)
             
             if key == 'q':
                 set_mode("OFFBOARD")
             if key == 'e':
                 arming(1)
+                
             if key == 'r':
                 pos_z = 0
                 set_mode("AUTO.LAND")   
+                init = True
             if key == 'm':
                 set_mode("MANUAL")
 
@@ -189,18 +274,30 @@ if __name__=="__main__":
                 pos_x += moveBindings[key][0] * 0.1
                 pos_y += moveBindings[key][1] * 0.1
                 pos_z += moveBindings[key][2] * 0.1
+                degree += moveBindings[key][3] * 5
                 if pos_z <= 0 : pos_z = 0
+                if degree > 180 : degree = 179
+                if degree <-180 : degree = -179
 
             else:
                 # stopped.
                 if (key == '\x03'):
                     break
                 
-            pub_thread.update(pos_x, pos_y, pos_z)
-            print(msg)
-            print('pos_x : ',pos_x)
-            print('pos_y : ',pos_y)
-            print('pos_z : ',pos_z)
+            
+            txt = ""
+            txt += 'x : ' + str(pos_x)
+            txt += ', y : ' + str(pos_y)
+            txt += ', z : ' + str(pos_z)
+            txt += ', deg : ' + str(pub_thread.degree)
+            print("\r                                                    ", end="")
+            print("\r" + txt, end="")
+            pub_thread.update(pos_x, pos_y, pos_z, degree)
+
+  
+            #print('degree : ',mav_status_turn_deg)
+        
+ 
 
     except Exception as e:
         print(e)
@@ -209,5 +306,4 @@ if __name__=="__main__":
 
         pub_thread.stop()
         termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-
 
